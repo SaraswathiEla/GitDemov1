ed910166a3f1375e24a23c9903b1dd3a57e73001
Saraswathi B <Saraswathi B@DESKTOP-C8SIN83>
2022-12-26 14:30:24 +0530

    first commit...

---
+++ src/d/Test1.java
@@ -0,0 +1,17 @@
+package d;
+
+public  class Test1 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes") {
+			System.out.println("eat something");
+		}
+		else {
+			System.out.print("do something");
+		}
+		
+
+	}
+}
+
